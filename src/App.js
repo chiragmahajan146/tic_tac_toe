857aab597d80da712d6b75dec5c2ee2442b23840
chiragmahajan146 <chiragmahajan146@gmail.com>
2021-05-26 23:00:49 +0530

    added buttons and styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from "react";
-
-export default () => (
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+import React from 'react';
+import Board from './components/Board';
+import './Styles/root.scss';
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Tic TAc Toe</h1>
+      <Board />
+    </div>
+  );
+};
+export default App;
